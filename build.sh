996c71746cc23b3a76710550b70bfbb8eea0b933
rpenumalli <47223659+rpenumalli@users.noreply.github.com>
2019-03-06 20:03:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 JMETER_VERSION="4.0"
 JMETER_IMG_TAG="latest"
-
-
 # Example build line
 # --build-arg IMAGE_TIMEZONE="Europe/Amsterdam"
 sudo docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} -t "jmeter4:${JMETER_IMG_TAG}" .
